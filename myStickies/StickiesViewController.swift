83011a05468ac16592b75e3786ea9a11bd49bc38
Milos Jovanovic <Milos-20@hotmail.com>
2019-05-01 16:27:41 +0200

    Datasource and delegate methods complete.

---
+++ myStickies/StickiesViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  myStickies
+//
+//  Created by Milos Jovanovic on 30/04/2019.
+//  Copyright © 2019 Milos Jovanovic. All rights reserved.
+//
+
+import UIKit
+
+class StickiesViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyStickiesItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
