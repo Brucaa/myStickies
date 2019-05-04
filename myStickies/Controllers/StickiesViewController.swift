e31f1428b1ca71965a67d1b7d20bf2a891a0c3e1
Milos Jovanovic <Milos-20@hotmail.com>
2019-05-04 13:35:19 +0200

    Persisting data using Codable protocol and Plist.

--- myStickies/Controllers/StickiesViewController.swift
+++ myStickies/Controllers/StickiesViewController.swift
@@ -12,28 +12,19 @@ class StickiesViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "StickiesArray") as? [Item] {
-            itemArray = items
-        }
+        print(dataFilePath)
+        loadItems()
     }
+    
+    
+    //MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -53,15 +44,14 @@ class StickiesViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-//        print(itemArray[indexPath.row])
-        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.reloadData()
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //MARK - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -77,9 +67,8 @@ class StickiesViewController: UITableViewController {
             newItem.title = textField.text! //Seting title property
             self.itemArray.append(newItem) //Appending item to itemArray
             
-            self.defaults.set(self.itemArray, forKey: "StickiesArray") //seting into UserDefaults using key StickiesArray
+            self.saveItems()
             
-            self.tableView.reloadData()
         }
         
         alert.addTextField { (alertTextField) in
@@ -92,6 +81,35 @@ class StickiesViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    //MARK - Model Manipulation Methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array,\(error)")
+            }
+        }
+    }
 
+    
+    
+    
 }
+
 
