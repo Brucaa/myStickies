b78beacfc09a23def6f3f1ad0a8e3b7f851d3b2a
Milos Jovanovic <Milos-20@hotmail.com>
2019-05-10 17:03:31 +0200

    Implemented swipe in CategoriesViewController.

--- myStickies/Controllers/CategoryViewController.swift
+++ myStickies/Controllers/CategoryViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     
@@ -19,6 +19,7 @@ class CategoryViewController: UITableViewController {
         super.viewDidLoad()
         
         loadCategories()
+        tableView.rowHeight = 80.0
 
     }
     
@@ -30,7 +31,7 @@ class CategoryViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath   )
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories added yet"
         
@@ -76,6 +77,19 @@ class CategoryViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error cant delete category, \(error)")
+            }
+        }
+    }
     
     //MARK: - Add New Categories
     
@@ -109,7 +123,7 @@ class CategoryViewController: UITableViewController {
     
     }
     
-
-    
     
 }
+
+
